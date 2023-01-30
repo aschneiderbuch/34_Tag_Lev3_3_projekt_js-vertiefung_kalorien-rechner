1717e2248dfa2b927720b21363e3624048742198
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-30 17:38:27 +0100

    htmlGerüst

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,45 @@
 console.log("test");
+
+/* 
+Frage
+
+Eingabe
+    Körpergewicht
+    Größe
+    Alter
+    Geschlecht   Mann / Frau
+
+Berechnung
+    Mann 
+        Geschlecht   Mann   + 664,7 Kalorin
+        Körpergewicht * 13,7
+        Größe * 5
+        Alter * 6,8
+           
+
+Berechnung 
+    Frau
+        Geschlecht   Frau   + 655,1 Kalorin
+        Körpergewicht * 9,6
+        Größe * 1,8
+        Alter * 4,7
+           
+
+
+PAL
+ Körperliche Belastung
+PAL-Faktor
+    Schlafen; 0,95
+    Nur Sitzen oder Liegen; 1,2
+    Ausschließlich sitzende Tätigkeit mit wenig oder keiner körperlichen Aktivität in der Freizeit, z.B. Büroarbeit; 1,5
+    Sitzende Tätigkeit mit zeitweilig gehender oder stehender Tätigkeit, z.B. Studierende, Fließbandarbeiter, Laboranten, Kraftfahrer; 1,7
+    Überwiegend gehende oder stehende Tätigkeit, z.B. Verkäufer, Kellner, Handwerker, Mechaniker, Hausfrauen; 1,9
+    Körperlich anstrengende berufliche Arbeit; 2,2
+
+
+
+    
+Ausgabe
+    Grundumsatz
+    Gesamtumsatz = Grundumsatz + aktivitäts Nivau (PAL)
+*/
