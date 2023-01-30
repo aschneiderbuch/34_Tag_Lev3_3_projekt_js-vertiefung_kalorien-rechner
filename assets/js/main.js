374e2f654c187852d0bc80f36782767b056578da
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-30 22:07:19 +0100

    werteZwischenFunktionenTauschen

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,4 +42,58 @@ PAL-Faktor
 Ausgabe
     Grundumsatz
     Gesamtumsatz = Grundumsatz + aktivitäts Nivau (PAL)
+
+    in kcal und kJ 
+        kj = 4,184 * kcal 
 */
+
+
+
+
+// Variablen
+let körpergröße = document.querySelector("#körpergröße");
+let alter = document.querySelector("#alter");
+let gewicht = document.querySelector("#gewicht");
+
+let radio_weiblich = document.querySelector("#weiblich");
+let radio_männlich = document.querySelector("#männlich");
+
+let liste = document.querySelector("#liste");
+
+let button = document.querySelector("#button");
+
+
+// EventListener
+button.addEventListener("click", werteBerechnen );
+
+liste.addEventListener("change", ausListeAuslesen );
+
+let wertAusListe = "";
+console.log(wertAusListe);
+
+ 
+// Funktion
+function ausListeAuslesen(event){
+    
+    console.log("event.target.value" + event.target.value);
+let wert = event.target.value; // Value 0,95 bis 2,2 von select Liste auslesen
+    wertAusListe = wert 
+    console.log("WertAusListe: " + wertAusListe);
+console.log("Wert: " + wert);
+return wert;
+};
+
+console.log(wertAusListe);    // hier kommt nur 0 an
+
+// Funktion 
+function werteBerechnen(){
+console.log("event Listener geht");
+console.log("wertAusliste: " + wertAusListe); // geht 0,95 bis 2,2
+
+let wert = ausListeAuslesen();
+console.log(wert);
+
+if (körpergröße.value >0 && alter.value > 0 && gewicht.value > 0 && (radio_männlich.checked || radio_weiblich.checked)){
+    console.log("prüfung geht ohne liste")};
+
+};
